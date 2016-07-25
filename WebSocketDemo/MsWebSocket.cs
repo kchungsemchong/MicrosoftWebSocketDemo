25d4a04001da424cf27675d09de1b53c8746e00f
kchungsemchong <kchungsemchong@gmail.com>
2016-07-25 09:47:06 +0400

    Initial Commit

---
+++ WebSocketDemo/MsWebSocket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebSockets;
+
+namespace WebSocketDemo
+{
+    public class MsWebSocket : WebSocketHandler
+    {
+        //The WebSocketColection allows the broadcast the message to all clients.
+        private static WebSocketCollection clients = new WebSocketCollection();
+
+        private string name;
+
+        public override void OnOpen()
+        {
+            this.name = this.WebSocketContext.QueryString["userName"];
+
+            //Add the client that just connected
+            clients.Add(this);
+
+            //Broadcast the newly connected user
+            clients.Broadcast(name + " is online");
+        }
+
+        public override void OnMessage(string message)
+        {
+            //When message comes in broadcast the message
+            clients.Broadcast(string.Format("{0} :{1}", name, message));
+        }
+
+        public override void OnClose()
+        {
+            //When user is disconnected. Message is broadcasted
+            clients.Remove(this);
+            clients.Broadcast(string.Format("{0} went offline", name));
+        }
+    }
+}
---
+++ WebSocketDemo/Server.ashx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Web.WebSockets;
+
+namespace WebSocketDemo
+{
+    public class Server : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+            {
+                context.AcceptWebSocketRequest(new MsWebSocket());
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
